48396a15bbcdd9a14522937482409e72ad497ff6
mpatberg50 <mrp5379@psu.edu>
2015-06-03 04:15:44 -0400

    Initial

---
+++ SudokuSolver/src/sudokusolver/SudokuSolver.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudokusolver;
+
+/**
+ *
+ * @author Patberg
+ */
+public class SudokuSolver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SudokoPuzzleGUI gui = new SudokuPuzzleGUI();
+        gui.
+    }
+    
+}
